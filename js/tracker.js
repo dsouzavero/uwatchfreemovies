d06dea7f2bd043d74faac3fb2e05873f65aa60e3
veigasjeff <154071076+dsouzavero@users.noreply.github.com>
2024-01-31 16:12:40 +0530

    Fresh Update

---
+++ js/tracker.js
@@ -0,0 +1,16 @@
+// googlescript.js
+
+(function () {
+    var script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-E46DVYM87Z';
+  
+    document.head.appendChild(script);
+  
+    script.onload = function () {
+      window.dataLayer = window.dataLayer || [];
+      function gtag() { dataLayer.push(arguments); }
+      gtag('js', new Date());
+      gtag('config', 'G-E46DVYM87Z');
+    };
+  })();
